f64ea96025b1942cd38cd1d8a3ee8f7354ccaccc
MGousseff <matthieu.gousseff@cnrs.fr>
2026-09-23 21:37:03 +0200

    trying to fix importLCZvect when LCZ levels hav A instead of 101

--- R/importLCZvect.R
+++ R/importLCZvect.R
@@ -151,12 +151,13 @@ importLCZvect <- function(dirPath, file = "bdt_lcz.fgb", output = "sfFile", colu
   }
 
   if (column != "") {
+    sfFile[[column]]<-as.character(sfFile[[column]])
     prov <- as.character(unique((st_drop_geometry(subset(sfFile, select = column, drop = T))))) %>% as.character
     names(prov) <- prov
-    if (prod(prov %in% typeLevels) == 0) {
+    if (prod(prov %in% names(typeLevels)) == 0) {
       if (verbose == TRUE) {
         print("levels in typeLevels are : ")
-        print(typeLevels)
+        print(names(typeLevels))
         print("levels in original data set are ")
         print(unique(subset(sfFile, select = column, drop = T)))
       }
@@ -167,7 +168,7 @@ importLCZvect <- function(dirPath, file = "bdt_lcz.fgb", output = "sfFile", colu
       )
 
     }
-    if (sum(prov %in% typeLevels) == 0) {
+    if (sum(prov %in% names(typeLevels)) == 0) {
       stop(
         paste0("none of the levels present in ", column,
                " is covered by the levels you specified.",
@@ -178,6 +179,7 @@ importLCZvect <- function(dirPath, file = "bdt_lcz.fgb", output = "sfFile", colu
 
     # Recode the values using the named vector
     sfFile[[column]] <- typeLevels[sfFile[[column]]]
+    names(sfFile[[column]]) <- sfFile[[column]]
 
     # Convert to factor with the correct levels
     sfFile[[column]] <- factor(sfFile[[column]], levels = unique(typeLevels))
